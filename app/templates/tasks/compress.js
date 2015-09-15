79be0dfda2e8fd1fdac992d016350bfc4775493a
HaNdTriX <handtrix@gmail.com>
2015-09-15 15:00:05 +0200

    Added logging to compress task

--- app/templates/tasks/compress.js
+++ app/templates/tasks/compress.js
@@ -1,4 +1,5 @@
 import gulp from 'gulp';
+import gutil from 'gulp-util';
 import zip from 'gulp-zip';
 import packageDetails from '../package.json';
 
@@ -6,7 +7,12 @@ gulp.task('compress', () => {
   let name = packageDetails.name;
   let version = packageDetails.version;
   let filename = `${name}-${version}.zip`;
-  gulp.src('dist/**/*')
+  gulp.src('dist/*')
     .pipe(zip(filename))
-    .pipe(gulp.dest('./packages'));
+    .pipe(gulp.dest('./packages'))
+    .on('end', () => {
+      let distStyled = gutil.colors.gray(`./dist`);
+      let filenameStyled = gutil.colors.gray(`./packages/${filename}`);
+      gutil.log(`Compressed ${distStyled} to ${filenameStyled}`);
+    });
 });
