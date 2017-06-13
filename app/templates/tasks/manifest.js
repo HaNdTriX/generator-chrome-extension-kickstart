ea5422503375d187745f715c9788dd6ba1f70d7e
HaNdTriX <handtrix@gmail.com>
2017-06-13 16:54:46 +0200

    Improve manifes json warning close #16

--- app/templates/tasks/manifest.js
+++ app/templates/tasks/manifest.js
@@ -1,5 +1,6 @@
 import gulp from 'gulp'
 import gulpif from 'gulp-if'
+import { colors, log } from 'gulp-util'
 import livereload from 'gulp-livereload'
 import jsonTransform from 'gulp-json-transform'
 import plumber from 'gulp-plumber'
@@ -8,11 +9,17 @@ import args from './lib/args'
 
 gulp.task('manifest', () => {
   return gulp.src('app/manifest.json')
-    .pipe(plumber())
+    .pipe(plumber({
+      errorHandler: error => {
+        if (error) {
+          log('manifest:', colors.red('Invalid manifest.json'))
+        }
+      }
+    }))
     .pipe(
       jsonTransform(
-        applyBrowserPrefixesFor(args.vendor)
-        , 2 /* whitespace */
+        applyBrowserPrefixesFor(args.vendor),
+        2 /* whitespace */
       )
     )
     .pipe(gulp.dest(`dist/${args.vendor}`))
